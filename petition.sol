86dafcd83895fee3655434de77df2a2030f32c20
Pablo Santos <iphonepablosantos@gmail.com>
2024-10-04 21:59:33 +0100

    petition

---
+++ petition.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+contract petition {
+    
+    string[3] private names;
+    uint256 public nameCount;
+
+    constructor() {
+        nameCount = 0;
+    }
+
+    function registerName(string memory _name) public {
+        if (nameCount < 3){
+            names[nameCount] = _name;
+            nameCount++;
+        }
+    }
+
+    function getName(uint256 _id) public view returns (string memory) {
+        if (_id < 3) {
+            return names[_id];
+        }
+        return "Error";
+    }
+
+    function petitionFull() public view returns (bool) {
+        return nameCount >= 3;
+    }
+
+}
