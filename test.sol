07253c2112f8f9ed12f1990adae1b1adb39915a6
Pablo Santos <iphonepablosantos@gmail.com>
2024-10-20 12:25:46 +0100

    test

--- test.sol
+++ test.sol
@@ -2,20 +2,30 @@
 
 pragma solidity ^0.8.19;
 
-contract Persons {
+    contract Persons {
 
-    struct Person {
-        string name;
-        uint age;
-        bool brazilian;
+        struct person {
+            string name;
+            uint age;
+            bool brazilian;
+        }
+
+        person public persons;
+
+        constructor(string memory _name, uint _age, bool _brazilian) {
+            persons.name = _name;
+            persons.age = _age;
+            persons.brazilian = _brazilian;
+        }
+
+        function updatePerson(string memory _name, uint _age, bool _brazilian) public {
+            persons.name = _name;
+            persons.age = _age;
+            persons.brazilian = _brazilian;
+        }
+
+        function getPerson() public view returns (string memory, uint, bool) {
+            return (persons.name, persons.age, persons.brazilian);
+        }
+
     }
-
-    Person public person;
-
-    constructor(string memory _name, uint _age, bool _brazilian) {
-        person.name = _name;
-        person.age = _age;
-        person.brazilian = _brazilian;
-    }
-
-}
