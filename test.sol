44ad678c91de8d003f2bf8c332e4e0682ab55b3b
Pablo Santos <iphonepablosantos@gmail.com>
2024-10-20 12:10:04 +0100

    test

--- test.sol
+++ test.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.19;
 
 contract Persons {
 
-    struct person {
+    struct Person {
         string name;
         uint age;
         bool brazilian;
     }
+
+    Person public person;
 
     constructor() {
         person.name = "Pablo";
