b2e441fa8f910b9a2c3ffdb3c05a12dc58ea167c
Pablo Santos <iphonepablosantos@gmail.com>
2024-10-27 20:57:39 +0000

    att

--- test2.sol
+++ test2.sol
@@ -6,4 +6,8 @@ contract dono {
 
     address endereco = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 
+    function getValue() public view returns (uint) {
+        return endereco.balance;
+    }
+
 }
