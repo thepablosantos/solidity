8399ad7200e7d1833fd4d04aaa35fcd5c8569599
Pablo Santos <iphonepablosantos@gmail.com>
2024-12-22 19:19:31 +0000

    solidity

--- aula1.sol
+++ aula1.sol
@@ -1,15 +1,30 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.1;
+pragma solidity ^0.8.19;
 
-constructor pessoa {
+contract pessoa {
 
-string public nome;
-uint private idade;
-bool internal nacionalidade;
+    string public name;
+    uint private age;
+    bool internal brasileiro;
 
-function setname (string memory _nome) public {
-    nome = _nome;
-}
+    function setName(string memory _name) public {
+        name = _name;
+    }
 
+    function setAge(uint _age) public {
+        age = _age;
+    }
+
+    function getAge() public view returns (uint) {
+        return age;
+    }
+
+    function setNacionality (bool _brasileiro) public {
+        brasileiro = _brasileiro; 
+    }
+
+    function getNacionality() public view returns (bool) {
+        return brasileiro;
+    }
 }
