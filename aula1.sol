baab980c0d2359c6a18f24c9dbb57c76cae98f33
Pablo Santos <iphonepablosantos@gmail.com>
2024-12-24 17:45:51 +0000

    solidity

--- aula1.sol
+++ aula1.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.19;
 contract person {
 
     string public name;
-    uint public age;
+    uint private age;
     bool internal brazilian;
 
     constructor() {
@@ -13,15 +13,28 @@ contract person {
         name = "Pablo";
         age = 31;
         brazilian = true;
-
     }
 
-    function setNat(bool _brazilian) public {
+    function setName (string memory _name) public {
+        name = _name;
+    }
+
+    function setAge (uint _age) public {
+        age = _age;
+    }
+
+    function setBrazilian (bool _brazilian) public {
         brazilian = _brazilian;
     }
 
-    function getNat() public view returns (bool) {
+    function getAge() public view returns (uint) {
+        return age;
+    }
+
+    function getNacionality() public view returns (bool) {
         return brazilian;
     }
+
+    
 
 }
