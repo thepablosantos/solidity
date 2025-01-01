a8c38b779590383002a652fa54e7dd1d7f345e98
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-01 20:17:23 +0000

    mytoken

--- NewCoin.sol
+++ NewCoin.sol
@@ -1,23 +1,22 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.19;
 
-// Importando os contratos ERC20 e Ownable da Openzeppelin
 import "@openzeppelin/contracts/token/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 mapping(address => bool) private blacklist;
+
 event BlacklistUpdated(address indexed account, bool isBlackListed);
 
-// Definir nome do contrato, do token e simbolo, e mintar
 contract NewCoin is ERC20, ownable {
-    constructor() ERC20("Newcoin", "NCoin") {
-        _mint(msg.sender, 10000000 * 10 ** decimals());
+    constructor() ERC20("NewCoin", "NCoin") {
+        _mint(10000 * 10 ** decimals());
     }
 }
 
-function addToBlackList(address account) external onlyOwner {
+function addBlackList(address account) external onlyOwner {
     require(!Blacklist[account], "Address is already blacklisted");
     blacklist[account] = true;
-    emit BlacklistUpdated(account, true);
+    emit BlackllistUpdated(account, true);
 }
