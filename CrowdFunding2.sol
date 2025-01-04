e8b79f8287cd72fc8c5d88a6c4181c6e4d177b08
kachapuram <nagatejakachapuram@gmail.com>
2025-01-04 10:01:10 +0530

    Improved CrowdFunding contract: modularity and gas optimizations

--- CrowdFunding2.sol
+++ CrowdFunding2.sol
@@ -1,43 +1,92 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.9;
 
+/**
+ * @title CrowdFunding
+ * @dev A simple crowdfunding contract that allows users to fund and the owner to withdraw funds.
+ */
 contract CrowdFunding {
+    /// @notice Address of the contract owner
+    address public immutable owner;
 
-    address public owner;
-    address[] public funders;
-    mapping(address => uint256) public addressToAmountFundes;
-    event NewFunding(address _from, uint256 _value);
+    /// @notice Minimum amount required to fund the contract
+    uint256 public constant MINIMUM_FUND = 0.01 ether;
 
+    /// @notice Maps funder addresses to the amount they have funded
+    mapping(address => uint256) public addressToAmountFunded;
+
+    /// @dev List of all funders who have contributed
+    address[] private funders;
+
+    /// @dev Emitted when a new funding occurs
+    /// @param funder The address of the funder
+    /// @param amount The amount funded
+    event NewFunding(address indexed funder, uint256 amount);
+
+    /// @dev Emitted when the owner withdraws all funds
+    /// @param owner The address of the owner
+    /// @param amount The total amount withdrawn
+    event Withdrawal(address indexed owner, uint256 amount);
+
+    /**
+     * @dev Sets the deployer as the owner of the contract.
+     */
     constructor() {
         owner = msg.sender;
     }
 
+    /**
+     * @notice Ensures only the contract owner can call the function.
+     */
     modifier onlyOwner() {
-        require(msg.sender == owner, "Withdraw only by contract owner!");
+        require(msg.sender == owner, "Only contract owner can call this!");
         _;
     }
 
-    function fund() public payable {
-        funders.push(msg.sender);
+    /**
+     * @notice Allows users to fund the contract with ETH.
+     * @dev Adds the sender to the funders list if they are funding for the first time.
+     * Emits a {NewFunding} event.
+     */
+    function fund() external payable {
+        require(msg.value >= MINIMUM_FUND, "Minimum funding amount not met!");
 
-        addressToAmountFundes[msg.sender] += msg.value;
+        // Update storage only when necessary
+        if (addressToAmountFunded[msg.sender] == 0) {
+            funders.push(msg.sender);
+        }
+
+        addressToAmountFunded[msg.sender] += msg.value;
 
         emit NewFunding(msg.sender, msg.value);
     }
 
-    function withdraw() public payable onlyOwner {
+    /**
+     * @notice Allows the owner to withdraw all funds from the contract.
+     * @dev Transfers the contract's balance to the owner and resets funders' data.
+     * Emits a {Withdrawal} event.
+     */
+    function withdraw() external onlyOwner {
+        uint256 contractBalance = address(this).balance;
+        require(contractBalance > 0, "No funds available for withdrawal!");
 
-        payable(msg.sender).transfer(address(this).balance);
+        // Transfer funds to the owner
+        payable(owner).transfer(contractBalance);
+        emit Withdrawal(owner, contractBalance);
 
-        for(uint256 i = 0; i < funders.length; i++){
-            address addr = funders[i];
-
-            addressToAmountFundes[addr] = 0;
+        // Reset funders' data
+        for (uint256 i = 0; i < funders.length; i++) {
+            address funder = funders[i];
+            addressToAmountFunded[funder] = 0;
         }
-
-        funders = new address[](0);
+        delete funders; // Efficiently clear the funders array
     }
 
-
-} //
+    /**
+     * @notice Retrieves the list of all funders.
+     * @return An array of funder addresses.
+     */
+    function getFunders() external view returns (address[] memory) {
+        return funders;
+    }
+}
